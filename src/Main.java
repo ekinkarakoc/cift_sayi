8319a5e33bece9f7d592988518a40321c22834bf
ekin <ekinkarakoc1@gmail.com>
2024-02-10 02:48:53 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Sayi giriniz: ");
+        k = scanner.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            if(i%2==0) {
+                System.out.println(i);
+            }
+        }
     }
 }
